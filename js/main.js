8557b1870f0cb17bd4774b650425e786280d7ed1
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-05-06 09:33:04 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// const alpha = (para) => {
+//   const a = "abcdefghijklmnopqrstuvwxyz";
+//    const str = str2 => Math.abs(a.indexOf(para) - a.indexOf(str2));
+//    return "aeiou".split("").sort((a, b) => str(a) - str(b))[0];
+//  }
+//  console.log(alpha("v"));
+
+// function sortalph(a, b){
+//   let str = ["a", "e", "u", "i", "o"];
+//   return a.split('').filter(item => str.includes(item)).slice(0, b).join('');
+// }
+// console.log(sortalph("major league", 5));
